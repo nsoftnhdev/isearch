54991a6e6d82d3e597eec726d98afc87b19972bb
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-03-23 00:42:41 +0800

    updated

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -18,9 +19,13 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <AppContextProvider>
         <html lang="en">
-          <body
-            className={`${inter.className} antialiased`}
-          >
+          <body className={`${inter.className} antialiased`}>
+            <Toaster
+              toastOptions={{
+                success: { style: { background: "black", color: "white" } },
+                error: { style: { background: "black", color: "white" } },
+              }}
+            />
             {children}
           </body>
         </html>
